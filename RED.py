edb9ea200639e44a2eab446312dbb3d84ad9bcdf
EC2 Default User <ec2-user@ip-172-31-2-251.ap-south-1.compute.internal>
2024-05-28 09:05:30 +0000

    2

--- RED.py
+++ RED.py
@@ -1,3 +1,7 @@
 import subprocess
 
-subprocess.run(['git', 'init'])
+subprocess.run(['cd', '/home/ec2-user/git_demo'])
+# subprocess.run(['aws', 's3', 'cp', 'README.md', 's3://snowflake-lld-tcs-logo'])
+subprocess.run(['git', 'add', 'README.md'])
+subprocess.run(['git', 'commit', '-m', '"add"'])
+subprocess.run(['git', 'push'])
